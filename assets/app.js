8b0e3e08798e4d89611ac0d5acf360b39485c621
Ellen <107266328+Izillen@users.noreply.github.com>
2026-04-16 12:39:19 -0300

    Ajuste no status e botão concluir

--- assets/app.js
+++ assets/app.js
@@ -44,10 +44,10 @@ function adicionarNaLista(tarefa) {
         <div class="d-flex justify-content-between align-items-center">
             <div>
                 <strong>${tarefa.nome}</strong><br>
-                <small>Status: <span class="status badge ${(tarefa.status !== "Concluído" ? "text-bg-success" : "text-bg-info")} ">${tarefa.status}</span></small>
+                <small><span class="status badge ${(tarefa.status !== "Concluído" ? "text-bg-info" : "text-bg-success")} ">${tarefa.status}</span></small>
             </div>
             ${tarefa.status !== "Concluído" 
-                ? `<button class="btn btn-success btn-sm btn-concluir">Concluir</button>` 
+                ? `<button class="btn btn-primary btn-sm btn-concluir">Concluir</button>` 
                 : ""}
         </div>
     `;
